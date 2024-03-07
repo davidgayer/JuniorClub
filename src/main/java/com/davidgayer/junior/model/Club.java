09e3db7070104831e67da83963274780fca24550
DavidGayer <davidgayer1@gmail.com>
2024-03-07 08:38:20 +0100

    Created Club entity

---
+++ src/main/java/com/davidgayer/junior/model/Club.java
@@ -0,0 +1,37 @@
+package com.davidgayer.junior.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "clubs")
+public class Club {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
+    private Long id;
+    private String title;
+    private String imageUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+    
+}
