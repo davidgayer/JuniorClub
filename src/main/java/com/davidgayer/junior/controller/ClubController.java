84ab88e7707f6f940da6791c3e714287fa510c0d
DavidGayer <davidgayer1@gmail.com>
2024-03-07 15:51:59 +0100

    Created Club controller

---
+++ src/main/java/com/davidgayer/junior/controller/ClubController.java
@@ -0,0 +1,32 @@
+package com.davidgayer.junior.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.davidgayer.junior.service.ClubService;
+
+import com.davidgayer.junior.dto.ClubDto;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequiredArgsConstructor
+public class ClubController {
+
+    private final ClubService clubService;
+
+    @GetMapping("/clubs")
+    public String ListOfClubs(Model model) {
+        List<ClubDto> clubs = clubService.findAllClubs();
+        model.addAttribute("clubs", clubs);
+        return "clubs-list";
+    }
+    
+
+    
+}
