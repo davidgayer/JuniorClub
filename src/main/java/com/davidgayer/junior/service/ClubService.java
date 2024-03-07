5718840137365caa4dd5b5fd77f136bcaac5b5dd
DavidGayer <davidgayer1@gmail.com>
2024-03-07 15:55:13 +0100

    Created Club service interface

---
+++ src/main/java/com/davidgayer/junior/service/ClubService.java
@@ -0,0 +1,15 @@
+package com.davidgayer.junior.service;
+
+import java.util.List;
+import com.davidgayer.junior.dto.ClubDto;
+
+
+
+public interface ClubService {
+    List<ClubDto> findAllClubs();
+
+
+
+
+    
+}
