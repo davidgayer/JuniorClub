b0399f84d0e49d1ef60d02f360a4a2d34ec7b1a4
DavidGayer <davidgayer1@gmail.com>
2024-03-13 20:59:05 +0100

    created method for save new club

--- src/main/java/com/davidgayer/junior/service/ClubService.java
+++ src/main/java/com/davidgayer/junior/service/ClubService.java
@@ -2,14 +2,13 @@ package com.davidgayer.junior.service;
 
 import java.util.List;
 import com.davidgayer.junior.dto.ClubDto;
+import com.davidgayer.junior.model.Club;
 
 
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
-
-
-
-
-    
+    Club saveNewClub(ClubDto clubDto);    
 }
+
+
