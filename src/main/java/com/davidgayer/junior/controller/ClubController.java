08cf60b6e7f7b546ac7ea32cadb86d9830056d55
DavidGayer <davidgayer1@gmail.com>
2024-03-13 12:59:24 +0100

    Added ReqiestMapping at path /clubs and created controller for creation of new club

--- src/main/java/com/davidgayer/junior/controller/ClubController.java
+++ src/main/java/com/davidgayer/junior/controller/ClubController.java
@@ -11,19 +11,29 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping(path = "/clubs")
 public class ClubController {
 
     private final ClubService clubService;
 
-    @GetMapping("/clubs")
+    @GetMapping()
     public String ListOfClubs(Model model) {
         List<ClubDto> clubs = clubService.findAllClubs();
         model.addAttribute("clubs", clubs);
         return "clubs-list";
+    }
+
+    @GetMapping("/new")
+    public String createClubForm(Model model) {
+        ClubDto clubDto = new ClubDto();
+        model.addAttribute("club", clubDto);
+        return "clubs-create";
+
     }
     
 
