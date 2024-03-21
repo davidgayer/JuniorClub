6ade258edde76f7486cccc40041ec0eb794fc356
DavidGayer <davidgayer1@gmail.com>
2024-03-21 08:45:06 +0100

    Created validation for ClubDto attributes

--- src/main/java/com/davidgayer/junior/dto/ClubDto.java
+++ src/main/java/com/davidgayer/junior/dto/ClubDto.java
@@ -2,6 +2,7 @@ package com.davidgayer.junior.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,8 +15,11 @@ import lombok.NoArgsConstructor;
 public class ClubDto {
   
     private Long id;
+    @NotBlank(message = "Club title cannot be empty.")
     private String title;
+    @NotBlank(message = "Image URL cannot be empty.")
     private String imageUrl;
+    @NotBlank(message = "Content cannot be empty.")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
