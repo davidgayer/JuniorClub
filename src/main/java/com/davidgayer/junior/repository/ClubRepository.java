21323306e120444093931297f8b690e6e8d7e9a1
DavidGayer <davidgayer1@gmail.com>
2024-03-21 08:45:39 +0100

    Created repository method for search club

--- src/main/java/com/davidgayer/junior/repository/ClubRepository.java
+++ src/main/java/com/davidgayer/junior/repository/ClubRepository.java
@@ -1,11 +1,17 @@
 package com.davidgayer.junior.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.davidgayer.junior.model.Club;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Long>{
+    
+    @Query("SELECT c FROM Club WHERE c.title LIKE CONCAT('%', :query, '%')")
+    List<Club> searchClub(String query);
     
 }
