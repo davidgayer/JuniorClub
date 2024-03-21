25bf69fb2f9e6108d1cb22d65d0f9b3cd6650e66
DavidGayer <davidgayer1@gmail.com>
2024-03-22 00:22:49 +0100

    Mapped events to the club

--- src/main/java/com/davidgayer/junior/model/Club.java
+++ src/main/java/com/davidgayer/junior/model/Club.java
@@ -1,14 +1,19 @@
 package com.davidgayer.junior.model;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,14 +29,22 @@ import lombok.NoArgsConstructor;
 public class Club {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String imageUrl;
     private String content;
+
     @CreationTimestamp
+    @DateTimeFormat(pattern = "dd.MM.yyyy, HH:mm")
     private LocalDateTime createdOn;
+
     @UpdateTimestamp
+    @DateTimeFormat(pattern = "dd.MM.yyyy, HH:mm" )
     private LocalDateTime updatedOn;
-    
+
+    @OneToMany(mappedBy = "club", cascade = CascadeType.ALL)
+    @Builder.Default
+    private Set<Event> events = new HashSet<>();
+
 }
