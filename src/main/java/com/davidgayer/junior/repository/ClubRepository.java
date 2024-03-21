b26a38f2dc451f8c12c1b2b6ba03855ba40cd1e2
DavidGayer <davidgayer1@gmail.com>
2024-03-21 22:50:54 +0100

    Correct the query at searchClubs method

--- src/main/java/com/davidgayer/junior/repository/ClubRepository.java
+++ src/main/java/com/davidgayer/junior/repository/ClubRepository.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.davidgayer.junior.model.Club;
 
 @Repository
-public interface ClubRepository extends JpaRepository<Club, Long>{
-    
-    @Query("SELECT c FROM Club WHERE c.title LIKE CONCAT('%', :query, '%')")
-    List<Club> searchClub(String query);
-    
+public interface ClubRepository extends JpaRepository<Club, Long> {
+
+    @Query("SELECT c FROM Club c WHERE c.title LIKE CONCAT('%', :query, '%')")
+    List<Club> searchClubs(@Param("query") String query);
 }
