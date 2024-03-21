30d1a6649370af82a482e62beac3261884534a2d
DavidGayer <davidgayer1@gmail.com>
2024-03-21 08:43:58 +0100

    Added functionality of save, edit and delete the club and searching functionality

--- src/main/java/com/davidgayer/junior/controller/ClubController.java
+++ src/main/java/com/davidgayer/junior/controller/ClubController.java
@@ -2,6 +2,8 @@ package com.davidgayer.junior.controller;
 
 import com.davidgayer.junior.service.ClubService;
 
+import jakarta.validation.Valid;
+
 import com.davidgayer.junior.dto.ClubDto;
 
 import lombok.RequiredArgsConstructor;
@@ -10,10 +12,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -29,16 +35,61 @@ public class ClubController {
         return "clubs-list";
     }
 
+    @GetMapping("/{clubId}/detail")
+    public String clubDetail(@PathVariable("clubId") Long clubId, Model model) {
+        ClubDto clubDto = clubService.findById(clubId);
+        model.addAttribute("club", clubDto);
+        return "clubs-detail";
+    }
+
+    @GetMapping("/search")
+    public String searchClub(@RequestParam String query, Model model) {
+        List<ClubDto> clubsDto = clubService.searchClub(query);
+        model.addAttribute("club", clubsDto);
+        return "clubs-detail";
+    }
+
     @GetMapping("/new")
     public String createClubForm(Model model) {
         ClubDto clubDto = new ClubDto();
-        model.addAttribute("newClub", clubDto);
+        model.addAttribute("club", clubDto);
         return "clubs-create";
     }
 
     @PostMapping("/saveClub")
-    public String saveClub(@ModelAttribute("newClub") ClubDto clubDto) {
+    public String saveClub(@Valid @ModelAttribute("club") ClubDto clubDto, 
+                           BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "clubs-create";
+        }
         clubService.saveNewClub(clubDto);
-        return "success";
+        return "redirect:/clubs";
     }
+
+    @GetMapping("/{clubId}/edit")
+    public String editClubForm(Model model, @PathVariable Long clubId) {
+        ClubDto clubDto = clubService.findById(clubId);
+        model.addAttribute("club", clubDto);
+        return "clubs-edit";
+    }
+
+    @PostMapping("/{clubId}/edit")
+    public String postMethodName(@PathVariable Long clubId, 
+                                 @Valid @ModelAttribute("club") ClubDto clubDto,
+                                 BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "clubs-edit";
+        }
+        clubDto.setId(clubId);
+        clubService.saveEditedClub(clubDto);
+        return "redirect:/clubs";
+    }
+
+    @GetMapping("/{clubId}/delete")
+    public String deleteClub(@PathVariable Long clubId) {
+        clubService.deleteById(clubId);
+        return "redirect:/clubs";
+    }
+    
+
 }
