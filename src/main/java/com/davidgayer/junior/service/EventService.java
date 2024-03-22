0eae9928e3b3cadd1339110316510397b1c193ca
DavidGayer <davidgayer1@gmail.com>
2024-03-22 21:35:44 +0100

    Created methods for Event service interface

--- src/main/java/com/davidgayer/junior/service/EventService.java
+++ src/main/java/com/davidgayer/junior/service/EventService.java
@@ -1,5 +1,18 @@
 package com.davidgayer.junior.service;
 
+import java.util.List;
+
+import com.davidgayer.junior.dto.EventDto;
+import com.davidgayer.junior.model.Event;
+
 public interface EventService {
+
+    List<EventDto> getAllEvents();
+    List<EventDto> getAllClubsEvents();
+    Event findById(Long id);
+    Event saveNewEvent(EventDto eventDto);
+    Event saveEditedEvent(EventDto eventDto);
+    void deleteById(Long id);
+    Event searchEvent(String query);
     
 }
