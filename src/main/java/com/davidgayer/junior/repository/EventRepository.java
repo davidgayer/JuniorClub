299051c2c1ccf8f173d6e654762ccc79a77f6c17
DavidGayer <davidgayer1@gmail.com>
2024-03-23 10:15:13 +0100

    Created repository methods for search event and get event by club's id

--- src/main/java/com/davidgayer/junior/repository/EventRepository.java
+++ src/main/java/com/davidgayer/junior/repository/EventRepository.java
@@ -1,9 +1,18 @@
 package com.davidgayer.junior.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.davidgayer.junior.model.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT c FROM Event c WHERE c.title LIKE CONCAT('%', :query, '%')")
+    List<Event> searchEvents(@Param("query") String query);
+
+    List<Event> findAllByClubId(Long clubId);
     
 }
