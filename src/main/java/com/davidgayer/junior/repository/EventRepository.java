c1331fea82b1ce6a0ac452e3a89557919d521e48
DavidGayer <davidgayer1@gmail.com>
2024-03-27 18:42:11 +0100

    Corrected the query for search events

--- src/main/java/com/davidgayer/junior/repository/EventRepository.java
+++ src/main/java/com/davidgayer/junior/repository/EventRepository.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.davidgayer.junior.model.Event;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    @Query("SELECT c FROM Event c WHERE c.title LIKE CONCAT('%', :query, '%')")
+    @Query("SELECT c FROM Event c WHERE c.name LIKE CONCAT('%', :query, '%')")
     List<Event> searchEvents(@Param("query") String query);
 
     List<Event> findAllByClubId(Long clubId);
