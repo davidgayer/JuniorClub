6a37e24b98d4c8c4216dba8888c07bacc6d59b1e
DavidGayer <davidgayer1@gmail.com>
2024-04-11 15:54:34 +0200

    Created entity class for User and User role

---
+++ src/main/java/com/davidgayer/junior/model/Role.java
@@ -0,0 +1,33 @@
+package com.davidgayer.junior.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "role")    
+    private List<User> users = new ArrayList<>();
+    
+    
+}
