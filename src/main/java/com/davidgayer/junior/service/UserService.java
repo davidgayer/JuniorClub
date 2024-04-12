50a9673a1f4b84bf298677ff074a1a64834798a2
DavidGayer <davidgayer1@gmail.com>
2024-04-12 11:23:24 +0200

    Created UserService interface

---
+++ src/main/java/com/davidgayer/junior/service/UserService.java
@@ -0,0 +1,12 @@
+package com.davidgayer.junior.service;
+
+import com.davidgayer.junior.dto.RegistrationDto;
+
+public interface UserService {
+
+    void saveUser(RegistrationDto registrationDto);
+
+    
+
+    
+}
