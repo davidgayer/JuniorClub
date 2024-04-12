3acb352e78dc72d1fefbd773f3848ae06fb53078
DavidGayer <davidgayer1@gmail.com>
2024-04-12 11:24:39 +0200

    Added Constructors, getters and setters using Lombok

--- src/main/java/com/davidgayer/junior/dto/RegistrationDto.java
+++ src/main/java/com/davidgayer/junior/dto/RegistrationDto.java
@@ -2,7 +2,13 @@ package com.davidgayer.junior.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationDto {
 
     private Long id;
