97609b361f9311da3bfdb4f924c53af445b15f7d
DavidGayer <davidgayer1@gmail.com>
2024-04-12 13:04:23 +0200

    Created Controller for handling authentication, created register methods

---
+++ src/main/java/com/davidgayer/junior/controller/AuthController.java
@@ -0,0 +1,59 @@
+package com.davidgayer.junior.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.davidgayer.junior.dto.RegistrationDto;
+import com.davidgayer.junior.model.User;
+import com.davidgayer.junior.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterForm(Model model) {
+        RegistrationDto user = new RegistrationDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@Valid @ModelAttribute("user") RegistrationDto user, 
+                           BindingResult bindingResult, Model model) {
+
+        User existingUserEmail = userService.findByEmail(user.getEmail());
+        User existingUserUserName = userService.findByUsername(user.getUserName());
+
+        if (existingUserEmail != null && existingUserEmail.getEmail() != null && !existingUserEmail.getEmail().isEmpty()) {
+            bindingResult.rejectValue("email", null, "User with this email is already registered");
+        }
+
+        if (existingUserUserName != null && existingUserUserName.getUsername() != null && !existingUserUserName.getUsername().isEmpty()) {
+            bindingResult.rejectValue("userName", null, "This username is already taken.");
+        }
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("user", user);
+            return "register";
+        }
+
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+    
+}
